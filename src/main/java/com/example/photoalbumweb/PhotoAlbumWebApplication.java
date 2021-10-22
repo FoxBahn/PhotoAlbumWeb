10c35073577131d474ae626f7eeea4a3ecb7989b
FoxBahn <philgoofy@gmail.com>
2021-10-22 12:13:44 +0200

    Initial commit

---
+++ src/main/java/com/example/photoalbumweb/PhotoAlbumWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.photoalbumweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAlbumWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAlbumWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/photoalbumweb/PhotoAlbumWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.photoalbumweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAlbumWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
