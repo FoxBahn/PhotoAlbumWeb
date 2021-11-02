4bf24370e294d9c56f9280dba05782da56c4930b
FoxBahn <philgoofy@gmail.com>
2021-11-03 01:54:44 +0200

    Added List ect.. update all files

--- src/main/java/com/example/photoalbumweb/controller/UserController.java
+++ src/main/java/com/example/photoalbumweb/controller/UserController.java
@@ -3,12 +3,14 @@ package com.example.photoalbumweb.controller;
 import com.example.photoalbumweb.model.User;
 import com.example.photoalbumweb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/")
 
